cf7fdabc9a027b8d15007b218856c8fdfe5dece8
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-07-08 14:08:22 +0300

    Upgrade setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name='flet_lite',
-    version='1.5',
+    version='1.6',
     author='SKbarbon',
     description='A tiny version of flet to work on mobile development',
     long_description=long_descibe,
